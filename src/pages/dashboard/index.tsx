274f10247f87eeb44d31da84052dfec75101dae6
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-04-19 20:00:15 +0100

    spacing on dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -42,11 +42,11 @@ const Dashboard: React.FunctionComponent<LoginRequiredParams> = ({
 
   return (
     <LoginRequired loginRequired={loginRequired}>
-      <div className="mt-12 mb-32 max-w-screen-xl w-full mx-auto">
-        <div className="grid grid-cols-2 gap-12">
+      <div className="mb-32 max-w-screen-xl w-full mx-auto">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-14">
           {!isEmpty(progress) && (
-            <div className="flex flex-col space-y-2">
-              <h2 className="text-xl pb-2 mb-3 border-b border-gray-200 dark:border-gray-800">
+            <div className="flex flex-col space-y-2 mt-8">
+              <h2 className="text-xl font-semibold pb-2 mb-3 border-b border-gray-200 dark:border-gray-800">
                 Continue Learning
               </h2>
               {progress.map((item: any) => {
@@ -61,14 +61,14 @@ const Dashboard: React.FunctionComponent<LoginRequiredParams> = ({
             </div>
           )}
           <div>
-            <h1 className="text-xl pb-2 mb-3 border-b border-gray-200 dark:border-gray-800">
+            <h1 className="mt-8 text-xl font-semibold pb-2 mb-3 border-b border-gray-200 dark:border-gray-800">
               Bookmarks
             </h1>
             <Bookmarks viewer={viewer} />
           </div>
         </div>
-        <div className="mt-16">
-          <h1 className="text-xl pb-2 mb-3 border-b border-gray-200 dark:border-gray-800">
+        <div className="mt-20">
+          <h1 className="text-xl font-semibold pb-2 mb-3 border-b border-gray-200 dark:border-gray-800">
             Watch History
           </h1>
           <Bookmarks viewer={viewer} />
