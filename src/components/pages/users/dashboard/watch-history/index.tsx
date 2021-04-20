03dd6beaa24a1d26d2cbc6c056314f6fb8d7ddab
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-04-20 19:35:34 +0100

    layout work on watch history

--- src/components/pages/users/dashboard/watch-history/index.tsx
+++ src/components/pages/users/dashboard/watch-history/index.tsx
@@ -3,6 +3,7 @@ import {FunctionComponent} from 'react'
 import Link from 'next/link'
 import {track} from 'utils/analytics'
 import {LessonResource} from 'types'
+import {convertTimeWithTitles} from 'utils/time-utils'
 import {Resource} from 'types'
 import Image from 'next/image'
 import {first, get, isEmpty, filter, uniq} from 'lodash'
@@ -67,7 +68,12 @@ const CompletedLesson: FunctionComponent<CompletedLessonProps> = ({
 
   const definedProgress = resource_progress || progress
 
-  const {is_completed} = definedProgress || {}
+  const {
+    completed_lesson_count = 0,
+    time_left = 0,
+    lesson_count = 0,
+    is_completed,
+  } = definedProgress || {}
 
   const completedLessonSlugs = get(
     definedProgress,
@@ -89,8 +95,8 @@ const CompletedLesson: FunctionComponent<CompletedLessonProps> = ({
   const image_url = square_cover_480_url || image_128_url
 
   return (
-    <div className="grid grid-cols-12 w-full items-center pb-4 border-b border-gray-200">
-      <div className="col-span-6 flex items-center flex-row md:space-x-4 space-x-2">
+    <div className="grid md:grid-cols-2 grid-cols-1 w-full items-center pb-4 border-b border-gray-200">
+      <div className="flex items-center flex-row md:space-x-4 space-x-2">
         {image_url && resource_path && (
           <Link href={resource_path}>
             <a
@@ -122,11 +128,39 @@ const CompletedLesson: FunctionComponent<CompletedLessonProps> = ({
           </a>
         </Link>
       </div>
-      <div className="col-span-3">
-        {series && (
-          <Link href={series?.url || '#'}>
+      <div className="flex items-center space-x-4">
+        {isInProgress ? (
+          <div>{convertTimeWithTitles(time_left)}</div>
+        ) : (
+          <div>
+            <CheckIcon />
+          </div>
+        )}
+        <div>{convertTimeWithTitles(duration)}</div>
+        <div className="w-60">
+          {series && (
+            <Link href={series?.url || '#'}>
+              <a
+                className="text-coolGray-500 dark:hover:text-blue-300 hover:text-blue-600"
+                onClick={() =>
+                  track(`clicked completed lesson`, {
+                    slug: slug,
+                    type: type,
+                    location: 'completed lesson (title)',
+                  })
+                }
+              >
+                <h3 className="text-sm items-center text-center">
+                  {series?.title}
+                </h3>
+              </a>
+            </Link>
+          )}
+        </div>
+        <div>
+          <Link href={instructor_url || '#'}>
             <a
-              className="text-coolGray-500 dark:hover:text-blue-300 hover:text-blue-600"
+              className="text-coolGray-700 dark:hover:text-blue-300 hover:text-blue-600"
               onClick={() =>
                 track(`clicked completed lesson`, {
                   slug: slug,
@@ -135,31 +169,34 @@ const CompletedLesson: FunctionComponent<CompletedLessonProps> = ({
                 })
               }
             >
-              <h3 className="text-sm flex items-center">{series?.title}</h3>
+              <h3 className="text-sm flex items-center">
+                {instructor.full_name}
+              </h3>
             </a>
           </Link>
-        )}
-      </div>
-      <div className="col-span-2">
-        <Link href={instructor_url || '#'}>
-          <a
-            className="text-coolGray-700 dark:hover:text-blue-300 hover:text-blue-600"
-            onClick={() =>
-              track(`clicked completed lesson`, {
-                slug: slug,
-                type: type,
-                location: 'completed lesson (title)',
-              })
-            }
-          >
-            <h3 className="text-sm flex items-center">
-              {instructor.full_name}
-            </h3>
-          </a>
-        </Link>
+        </div>
       </div>
     </div>
   )
 }
+
+const CheckIcon = () => (
+  <svg
+    className="text-green-600"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g fill="none">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm3.707-9.293a1 1 0 0 0-1.414-1.414L9 10.586 7.707 9.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4z"
+        fill="currentColor"
+      />
+    </g>
+  </svg>
+)
 
 export default WatchHistory
